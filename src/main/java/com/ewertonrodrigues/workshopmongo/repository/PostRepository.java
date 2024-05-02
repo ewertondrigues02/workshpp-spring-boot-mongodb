9f6aadd0212885aaa2497d2c06bd603d0ad738a8
ewertondrigues02 <ewertonrodrigues01@gmail.com>
2024-05-02 10:22:53 -0300

    Consulta simples com @Query

--- src/main/java/com/ewertonrodrigues/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/ewertonrodrigues/workshopmongo/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.ewertonrodrigues.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.ewertonrodrigues.workshopmongo.domain.Post;
@@ -10,6 +11,11 @@ import com.ewertonrodrigues.workshopmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 
+	@Query("{'title': {$regex: ?0, $options: 'i'}}")
+	List<Post> searchTitle(String text);
+	
 	List<Post> findByTitleContainingIgnoreCase(String text);
+	
+	
 	
 }
